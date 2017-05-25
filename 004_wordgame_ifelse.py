6dce6311efa07347b32570f4fae2127e9a2429e5
ottokangjian <kangjiankangjian@gmail.com>
2017-05-25 12:41:42 +0800

    004_random

--- 004_wordgame_ifelse.py
+++ 004_wordgame_ifelse.py
@@ -1,14 +1,25 @@
+import random
+secret = random.randint(1,10)
+
 print ("Welcome")
 print ("Let's guess a number")
 
 guess = input("input a number pls")
 guess_int = int(guess)
 
-if guess_int == 8:
-	print ("Bingo!")
-else:
-	if guess_int > 8:
-		print ("Too big")
+while guess_int != secret:
+
+
+	if guess_int == secret:
+		print ("Bingo!")
 	else:
-		print ("Too small")
+		if guess_int > secret:
+			print ("Too big")
+		else:
+			print ("Too small")
+			
+	guess = input("input a number pls")
+	guess_int = int(guess)
+
+			
 print ("Bye!")
